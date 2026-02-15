5c20edb0e6261dd6069bf1461f06ea10f3e7c8a0
TaurineMerge <plotnikov12341@gmail.com>
2026-02-15 15:30:11 +0300

    feat: add scroll area to layout

--- ui/src/components/layout/layout.tsx
+++ ui/src/components/layout/layout.tsx
@@ -1,13 +1,18 @@
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/layout/app-sidebar';
 import Header from './header';
+import { ScrollArea } from '../ui/scroll-area';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <Header />
-      <main>{children}</main>
+      <div className="flex flex-col h-screen w-full">
+        <ScrollArea>
+          <Header />
+          <main className="w-full overflow-y-auto overflow-x-hidden">{children}</main>
+        </ScrollArea>
+      </div>
     </SidebarProvider>
   );
 }
