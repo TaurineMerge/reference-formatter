933e81c510bc1e12a52d30dd45c4dc51c4e41b9e
TaurineMerge <plotnikov12341@gmail.com>
2026-02-15 15:31:09 +0300

    feat: add input page to main app function

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,13 @@
 import Layout from './components/layout/layout';
 import { AppContext } from './context';
+import InputPage from './pages/input-page';
 
 function App() {
   return (
     <AppContext>
-      <Layout></Layout>
+      <Layout>
+        <InputPage />
+      </Layout>
     </AppContext>
   );
 }
