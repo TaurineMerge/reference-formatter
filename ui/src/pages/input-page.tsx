abe97ff44ed8baf079e45defd1ca6a3707400dd3
TaurineMerge <plotnikov12341@gmail.com>
2026-02-16 01:39:37 +0300

    chore: style changes

--- ui/src/pages/input-page.tsx
+++ ui/src/pages/input-page.tsx
@@ -1,26 +1,24 @@
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardFooter } from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 import { Textarea } from '@/components/ui/textarea';
 import { SendHorizontal } from 'lucide-react';
 
 export default function InputPage() {
   return (
-    <section className="w-full flex flex-col items-center gap-8 mt-[25vh]">
+    <section className="w-full min-h-full flex flex-col items-center gap-8">
       <div>
         <p className="text-5xl font-bold">Format your references</p>
       </div>
-      <Card className="w-1/2 flex flex-col items-center gap-4 p-4">
-        <CardContent className="w-full p-0">
+      <Card className="w-1/2 shadow-accent hover:shadow-accent-hover">
+        <CardContent className="w-full px-4 py-0 flex flex-row items-end">
           <Textarea
             placeholder="Paste your entries here"
-            className="resize-none overflow-y-auto max-h-64 w-full outline-none border-none focus-visible:ring-0"
+            className="resize-none overflow-y-auto max-h-64 min-h-0 w-full outline-none border-none shadow-none focus-visible:ring-0 bg-transparent dark:bg-transparent dark:border-none dark:focus-visible:ring-0"
           />
-        </CardContent>
-        <CardFooter className="w-full flex justify-end">
-          <Button variant="outline" className="cursor-pointer">
+          <Button variant="secondary" className="cursor-pointer">
             <SendHorizontal />
           </Button>
-        </CardFooter>
+        </CardContent>
       </Card>
     </section>
   );
